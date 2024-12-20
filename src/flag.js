8c4699b7a544785fd970e33dff8f8f0956a8b69a
ayan4m1 <andrew@bulletlogic.com>
2024-12-20 13:20:22 -0500

    remove camera controls

--- src/flag.js
+++ src/flag.js
@@ -1,6 +1,6 @@
 import { TextureLoader } from 'three';
 import { useRef, useMemo, Fragment } from 'react';
-import { PerspectiveCamera, OrbitControls } from '@react-three/drei';
+import { PerspectiveCamera } from '@react-three/drei';
 import { useThree, useFrame, useLoader } from '@react-three/fiber';
 
 import vertexShader from './shaders/vertex.glsl';
@@ -8,7 +8,6 @@ import fragmentShader from './shaders/fragment.glsl';
 
 export default function Flag() {
   const meshRef = useRef();
-  const cameraRef = useRef();
   const { clock } = useThree();
   const texture = useLoader(TextureLoader, './flag.png');
   const uniforms = useMemo(() => {
@@ -27,13 +26,7 @@ export default function Flag() {
   return (
     <Fragment>
       <ambientLight intensity={1} />
-      <PerspectiveCamera
-        fov={30}
-        makeDefault
-        position={[0, 0, 3]}
-        ref={cameraRef}
-      />
-      <OrbitControls camera={cameraRef.current} />
+      <PerspectiveCamera fov={30} makeDefault position={[0, 0, 3]} />
       <mesh position={[-1.055, 0, 0]}>
         <cylinderGeometry args={[0.1, 0.1, 10, 64, 64]} />
         <meshPhysicalMaterial color="#b5c0c9" />
