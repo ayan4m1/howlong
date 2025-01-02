f5156fb5120552c7cd5957b65c00862cefd7bf47
ayan4m1 <andrew@bulletlogic.com>
2025-01-02 17:40:17 -0500

    handle case after it happens

--- src/countdown.js
+++ src/countdown.js
@@ -2,7 +2,8 @@ import {
   parseISO,
   intervalToDuration,
   interval,
-  formatDuration
+  formatDuration,
+  isAfter
 } from 'date-fns';
 import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
@@ -14,6 +15,11 @@ const inauguration = parseISO('2025-01-25T10:00:00-5');
 
 const getRemainingTime = () => {
   const now = Date.now();
+
+  if (isAfter(now, inauguration)) {
+    return null;
+  }
+
   const duration = intervalToDuration(interval(now, inauguration));
 
   return formatDuration(duration, {
@@ -38,8 +44,10 @@ export default function Countdown() {
       <Canvas style={{ width: 600, height: 400 }}>
         <Flag />
       </Canvas>
-      <h1 className="rainbow-word">We are all fucked in</h1>
-      <h2>{timeLeft}</h2>
+      <h1 className="rainbow-word">
+        We are all fucked{Boolean(timeLeft) && ' in'}
+      </h1>
+      {Boolean(timeLeft) && <h2>{timeLeft}</h2>}
     </main>
   );
 }
